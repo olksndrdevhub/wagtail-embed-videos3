25fca380f245210fd1c5505e1d87c49ff627533e
Oleksandr Romaniuk <47930925+olksndrdevhub@users.noreply.github.com>
2021-01-29 21:05:07 +0200

    Add support for Wagtail>=2.9

--- wagtail_embed_videos/models.py
+++ wagtail_embed_videos/models.py
@@ -23,7 +23,11 @@ from django.urls import reverse
 from django.core.files import File
 from django.core.files.temp import NamedTemporaryFile
 
-from wagtail.admin.utils import get_object_usage
+try:
+    from wagtail.admin.utils import get_object_usage
+except ImportError:
+    from wagtail.admin.models import get_object_usage
+    
 from wagtail.images.models import Image as WagtailImage
 from wagtail.images import get_image_model
 from wagtail.search import index
